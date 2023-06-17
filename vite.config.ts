5bda5735d93209a3532b16fc5ebba650f56cab15
jdmiguel <jaimedmiguel@gmail.com>
2023-06-17 12:45:17 +0100

    app setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vite/client" />
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import * as path from 'path';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 9000,
+    open: true,
+  },
+});
